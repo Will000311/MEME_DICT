34e4796806c55d93e6aae238298b8162a797586e
Will000311 <wv90080@gmail.com>
2025-03-12 16:48:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
+            "PRO": "Alguien bueno en algo"
             }
 
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
